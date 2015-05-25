9db263534acb82d403f706be3702bb684785061e
Artem Bogachev <bourbaki@me.com>
2015-05-25 14:03:08 +0300

    remove old code

--- community_metrics.py
+++ community_metrics.py
@@ -39,39 +39,9 @@ def node_wcc(x, S, V):
     tS, tV = nx.triangles(S, x), nx.triangles(V, x)
     vtS, vtV = number_of_triangle_nodes(S, x), number_of_triangle_nodes(V, x)
     vtV_S = vtV - vtS
-    # tV,vtV,nbV = clique_tr(V, x)
-    # tS,vtS,nbS = clique_tr(S, x)
-    # vtV_S = nbV - nbS
     result = 0.0 if tV == 0 else tS / tV * vtV / (S.number_of_nodes() - 1 + vtV_S)
     print("node: {}, tS: {}, tV: {}, vtS: {}, vtV: {}, vtV_S: {} wcc: {}".format(x, tS, tV, vtS, vtV, vtV_S, result))
     return result
     
 def wcc(S, V):
-    return mean(node_wcc(node, S, V) for node in S)
-
-def clique_tr(G,node):
-   nb = set(nx.all_neighbors(G, node))
-   tr,nodes = set(),set()
-   for n in nb:
-       nodes.add(n)
-       for n2 in nx.all_neighbors(G, n):
-           if n2 in nb:
-               tr.add(tuple(sorted([n,n2])))
-               nodes.add(n2)
-   return (len(tr),len(nodes),len(nb))    
-
-def old_wcc(S, V):
-    nodes = nx.nodes(S)
-    num_nodes_gr = len(nodes)
-    wcc0 = 0.
-    for node in nodes:
-        tV,vtV,nbV = clique_tr(V,node)
-        tS,vtS,nbS = clique_tr(S,node)
-        vtV_S = nbV - nbS
-        print("node: {0}, tS: {1}, tV: {2}, vtS: {3}, vtV: {4}, vtV_S: {5}".format(node, tS, tV, vtS, vtV, vtV_S))
-        if tV != 0:
-            wcc0 += (tS*1./tV) * (vtV/(num_nodes_gr-1+vtV_S))
-    return wcc0 / num_nodes_gr
-
-def new_clique(G, x):
-    return (nx.triangles(G, x), number_of_triangle_nodes(G, x), G.degree(x))
+    return mean(node_wcc(node, S, V) for node in S) 
